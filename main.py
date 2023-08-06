aaaa458850a26490bb167f25c0295b826369c0ba
sevkw <kwang0422@gmail.com>
2023-08-05 23:00:13 -0400

    finish tennis launcher

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import os
 LAUNCHER_URL = 'https://slingerbag.ca/collections/slinger-bag-packages/products/slam-pack'
 IN_STOCK = 'Add to cart'
 load_dotenv()
-SENDER_EMAIL = os.get("SENDER_EMAIL")
-SENDER_EMAIL_APP_PSWD = os.get("SENDER_EMAIL_APP_PSWD")
-TO_EMAIL = os.get("TO_EMAIL")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_EMAIL_APP_PSWD = os.getenv("SENDER_EMAIL_APP_PSWD")
+TO_EMAIL = os.getenv("TO_EMAIL")
 
 
 response = requests.get(LAUNCHER_URL)
@@ -20,11 +20,15 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 purchase_option = soup.find(name="span", attrs={"data-add-to-cart-text":True}).text.strip()
 
-# print(purchase_option)
 if purchase_option == IN_STOCK:
-    print("The launcher is back in stock!")
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=SENDER_EMAIL, password=SENDER_EMAIL_APP_PSWD)
+        connection.sendmail(
+            from_addr=SENDER_EMAIL, 
+            to_addrs=SENDER_EMAIL,
+            msg=f"Subject:Tennis Launcher Back in Stock!\n\n{IN_STOCK} at: {LAUNCHER_URL}"
+        )
 
 else:
     print(f"The launcher is {purchase_option}")
-
-
